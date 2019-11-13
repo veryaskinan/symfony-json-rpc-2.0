1f65e322275511bc5e5b841f825e9df2e74fa57c
fizruk <fizruk@mail.com>
2019-11-13 19:01:26 +0300

    get json request

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -2,16 +2,30 @@
 
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class IndexController
 {
-    public function index()
+    private $request;
+
+    public function index(Request $request)
     {
+        $this->request = $request;
+
+        $this->getJson();
+
         return new Response(
-            json_encode(['Hellow', 'World']),
+            $request->getContent(),
             Response::HTTP_OK,
             ['content-type' => 'application/json']
         );
+    }
+
+    private function getJson() {
+        if (0 === strpos($this->request->headers->get('Content-Type'), 'application/json')) {
+            $data = json_decode($this->request->getContent(), true);
+            $this->request->request->replace(is_array($data) ? $data : []);
+        }
     }
 }
